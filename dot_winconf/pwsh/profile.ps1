781fc9266f47878066e8b8b612ce5916db7661c5
l1ttleflyyy <l1ttleflyyy@users.noreply.github.com>
2025-10-12 00:19:09 -0700

    windows config update

--- dot_winconf/pwsh/profile.ps1
+++ dot_winconf/pwsh/profile.ps1
@@ -67,3 +67,16 @@ if (Get-Command oh-my-posh 2> $null) {
     }
     oh-my-posh init pwsh --config $omp_theme | invoke-expression
 }
+
+function jabba
+{
+    $fd3=$([System.IO.Path]::GetTempFileName())
+    $command="& '$Env:JABBA_HOME\bin\jabba.exe' $args --fd3 `"$fd3`""
+    & { $env:JABBA_SHELL_INTEGRATION="ON"; Invoke-Expression $command }
+    $fd3content=$(Get-Content $fd3)
+    if ($fd3content) {
+        $expression=$fd3content.replace("export ","`$env:").replace("unset ","Remove-Item env:") -join "`n"
+        if (-not $expression -eq "") { Invoke-Expression $expression }
+    }
+    Remove-Item -Force $fd3
+}
